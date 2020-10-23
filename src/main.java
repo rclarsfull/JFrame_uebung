d655df4006d871fa655d93eb6a8a2907b1020f83
rclarsfull <l.p.1999@live.de>
2020-10-23 16:52:05 +0200

    spielerei

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import javax.swing.JFrame;
+import java.util.Random;
 
 
 
@@ -16,9 +17,44 @@ public class main extends Canvas {
 
 
 
+
     }
     public void paint(Graphics g){
         //super.paint(g);
-        g.fillOval(200,100,200,200);
+        Random rand=new Random();
+        int x=200;
+        int speed=1;
+        int width=10;
+        int height=10;
+        boolean fallen=true;
+        while (true){
+            if (fallen){
+                while (x>100) {
+                    x--;
+
+                    g.fillOval(rand.nextInt(400), rand.nextInt(400), width, height);
+                    try {
+                        Thread.sleep(speed);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                fallen=false;
+            }else {
+
+                while (x < 200) {
+                    x++;
+                    g.fillOval(rand.nextInt(400), rand.nextInt(400), width, height);
+                    try {
+                        Thread.sleep(speed);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                fallen=true;
+            }
+
+        }
+
     }
 }
